7552ff1989a689c481d9f02fd7e6afc7371b7fe7
Fredy Roman Vazquez Geronimo <crazzy-rock@live.com.mx>
2018-12-12 15:52:17 -0600

    Configuracion de Cors y headers

--- api-ws-js/app.js
+++ api-ws-js/app.js
@@ -7,19 +7,25 @@ var app = express();
 
 //Cargar de rutas
 var user_routes = require('./routers/use');
+var animal_routes = require('./routers/animal');
 
 //middlewares de  body parse
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 
 //configuracion de cabeceras y cors
+app.use((req, resp, next) => {
+    resp.header('Access-Control-Allow-Origin', '*');
+    resp.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    resp.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');  
+    resp.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');    
+    next();
+});
 
 //rutas base
 app.use('/api', user_routes);
 
-app.get('/probando', (req, resp) => {
-    resp.status(200).send({message: 'Este el método probando'});
-});
+app.use('/api', animal_routes);
 
 module.exports = app;
-
+ 
